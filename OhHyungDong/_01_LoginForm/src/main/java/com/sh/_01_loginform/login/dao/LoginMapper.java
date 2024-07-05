e85db7e7aedd1b3b5275ce6d0a85d6a88680fad5
dhgudehd98 <ohd7150@nate.com>
2024-07-05 14:54:54 +0900

    feat : 회원가입 작성

---
+++ OhHyungDong/_01_LoginForm/src/main/java/com/sh/_01_loginform/login/dao/LoginMapper.java
@@ -0,0 +1,14 @@
+package com.sh._01_loginform.login.dao;
+
+import com.sh._01_loginform.login.dto.MemberDto;
+
+import java.util.Optional;
+
+public interface LoginMapper {
+
+    Optional<MemberDto> duplicateId(String id);
+
+    int registerMember(String memberEmail, String memberPassword,String memberName);
+
+
+}
