2242d9fb369cd421815e80fde4c1397b651a5375
nnieun <abocado216@naver.com>
2024-07-06 23:19:38 +0900

    UPdate : 회원가입폼 제출 알림창 수정 #9

--- KimNakyoung/sod/src/main/java/com/sh/sod/controller/MemberController.java
+++ KimNakyoung/sod/src/main/java/com/sh/sod/controller/MemberController.java
@@ -30,14 +30,15 @@ public class MemberController {
 
     //회원가입등록
     @PostMapping("/regist")
-    public String regist(@ModelAttribute MemberRegistDto memberRegistDto, RedirectAttributes redirectAttributes){
+    public String regist(@ModelAttribute MemberRegistDto memberRegistDto){
         log.info("POST /member/regist");
         log.debug("memberDto = {}", memberRegistDto);
         MemberDto memberDto = memberRegistDto.toMemberDto();
-        int result = memberCommandService.insertMember(memberDto);
-        if (result == 1) {
-            redirectAttributes.addFlashAttribute("message", "회원을 성공적으로 등록했습니다.");
-        }
+//        int result = memberCommandService.insertMember(memberDto);
+//        if (result == 1) {
+//            redirectAttributes.addFlashAttribute("message", "회원을 성공적으로 등록했습니다.");
+//        } // 이거 쓰려면 변수에 RedirectAttributes redirectAttributes 이거 쓰기 현재 뺐음
+        memberCommandService.insertMember(memberDto); // 현재 이 결과를 쓸 곳이 없어서 변수에 안 담음
         return "redirect:/"; // 현재 다른 창이 없어서 index 창으로 간다고 해놈
     }
 
