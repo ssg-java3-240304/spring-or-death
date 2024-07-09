2df4731de258f9fe2abbfa8dffd2e77ecca36272
Hong-ji-min <jmh907@naver.com>
2024-07-10 00:15:06 +0900

    Fix : 이메일 중복 체크 기능 완료 #15

--- HongJimin/Spring_or_death_app/src/main/java/spring/app/member/controller/MemberController.java
+++ HongJimin/Spring_or_death_app/src/main/java/spring/app/member/controller/MemberController.java
@@ -24,19 +24,19 @@ public class MemberController {
     }
 
     @PostMapping("/regist")
-    @ResponseBody
+//    @ResponseBody
     public String regist(@ModelAttribute MemberRegistDto memberRegistDto) {
         log.debug("/member/regist");
         log.debug("memberRegistDto = {}", memberRegistDto);
 
         MemberDto memberDto = memberRegistDto.toMemberDto();
         int result = memberCommandService.registMember(memberDto);
-        return "회원가입 축하드립니다!✨";
+        return "redirect:/";
     }
 
     @PostMapping("/sameEmailCheck")
     @ResponseBody // ajax를 쓰기 위해 이 어노테이션 써야 함
-    public int sameEmailCheck(@RequestParam("memberEmail") String memberEmail) {
+    public int sameEmailCheck(@RequestParam String memberEmail) {
         log.debug("/member/sameEmailCheck");
         log.debug("memberEmail = {}", memberEmail);
         int result = memberCommandService.checkSameMemberEmail(memberEmail);
