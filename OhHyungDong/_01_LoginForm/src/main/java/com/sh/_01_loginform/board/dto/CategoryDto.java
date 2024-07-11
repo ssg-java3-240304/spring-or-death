7bcb4d56487010d95a836722224e8e2c8072c793
dhgudehd98 <ohd7150@nate.com>
2024-07-11 18:22:33 +0900

    feat : 게시물 출력, 페이지 설정에 대한 로직 작성

---
+++ OhHyungDong/_01_LoginForm/src/main/java/com/sh/_01_loginform/board/dto/CategoryDto.java
@@ -0,0 +1,15 @@
+package com.sh._01_loginform.board.dto;
+
+import com.sh._01_loginform.login.dto.MemberDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor@NoArgsConstructor
+public class CategoryDto {
+    private int categoryCode;
+    private String categoryName;
+
+
+}
