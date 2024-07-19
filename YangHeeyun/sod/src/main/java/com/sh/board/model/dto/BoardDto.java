07c6cfb66e6707adaeda0b269dffa2903f4548c4
vlshzl35 <vlshzl35@naver.com>
2024-07-19 15:48:34 +0900

    Update: [오류] Dto에서 값을 매핑하지 못하는 중 ~ing #27

--- YangHeeyun/sod/src/main/java/com/sh/board/model/dto/BoardDto.java
+++ YangHeeyun/sod/src/main/java/com/sh/board/model/dto/BoardDto.java
@@ -1,16 +1,22 @@
 package com.sh.board.model.dto;
 
 import com.sh.member.model.dto.MemberDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardDto {
     private int boardNo;
     private int boardType;
-    private CategoryDto categoryCode;
+    private CategoryDto categoryDto; // categoryCode 가져올것
     private String boardTitle;
     private String boardBody;
-    private MemberDto memberId;
+    private MemberDto memberDto; // memberId 가져올것
     private int boardCount; // 조회수
     private LocalTime createdDate;
     private LocalTime modifiedDate;
--- YangHeeyun/sod/src/main/java/com/sh/board/model/dto/CategoryDto.java
+++ YangHeeyun/sod/src/main/java/com/sh/board/model/dto/CategoryDto.java
@@ -1,5 +1,12 @@
 package com.sh.board.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDto {
     private int categoryCode;
     private String categoryName;
